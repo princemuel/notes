47361fad86816ef2545b3c59ea431b2a294baba7
Prince Muel <vansomecsam@gmail.com>
2026-06-29 04:38:58 +0100

    ci: configure dev and prod native builds

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,9 +14,13 @@ export const auth = betterAuth({
   ],
   emailAndPassword: { enabled: true },
   socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
+    github: {
+        clientId: process.env.GITHUB_CLIENT_ID!,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      },
+      google: {
+        clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      },
   },
 });
