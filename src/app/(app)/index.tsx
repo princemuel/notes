65ac1ac86958fa7a9b99eb24891ce07e7bdfdc21
Prince Muel <vansomecsam@gmail.com>
2026-07-07 13:44:58 +0100

    build: update eas config for build workflow

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,14 +1,35 @@
 import { ScrollView } from "@expo/ui";
-import { Text, View } from "react-native";
+import { Fragment } from "react";
+import { FlatList, Text, View } from "react-native";
 
 import db from "@/lib/db.json";
 
 export default function Screen() {
-  console.log(db.notes);
   return (
     <ScrollView>
       <View className="">
-        <Text className="text-xl text-blue-500">React Native 🚀</Text>
+        <FlatList
+          data={db.notes}
+          renderItem={({ item }) => {
+            return (
+              <Fragment key={item.id}>
+                <Text>{item.title}</Text>
+
+                {item.tags.map((tag) => (
+                  <Text key={tag}>{tag}</Text>
+                ))}
+
+                <Text>
+                  {Temporal.Instant.from(item.created_at)
+                    .toZonedDateTimeISO(Temporal.Now.timeZoneId())
+                    .toLocaleString(undefined, { month: "short", day: "numeric", year: "numeric" })}
+                </Text>
+              </Fragment>
+            );
+          }}
+        />
+
+        <Text className="text-xl text-blue-500"></Text>
       </View>
     </ScrollView>
   );
