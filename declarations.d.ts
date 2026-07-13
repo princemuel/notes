b14c2f32afc341dde498fc4dfb4256cb718256f0
Prince Muel <vansomecsam@gmail.com>
2026-07-13 02:11:37 +0100

    chore: setup svg assets handling
    and users need the latest of these.

---
+++ declarations.d.ts
@@ -0,0 +1,27 @@
+declare module "*.svg" {
+  import React from "react";
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+declare module "*.gif" {
+  const content: string;
+  export default content;
+}
--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,1 +1,9 @@
-export const tabs = [{ name: "archived", title: "Archived", icon: "" }];
+import { ArchiveSVG, HomeSVG, SearchSVG, SettingsSVG, TagSVG } from "@/assets/icons";
+
+export const tabs = [
+  { name: "index", title: "Home", icon: HomeSVG },
+  { name: "search", title: "Search", icon: SearchSVG },
+  { name: "archived", title: "Archived", icon: ArchiveSVG },
+  { name: "tags", title: "Tags", icon: TagSVG },
+  { name: "settings", title: "Settings", icon: SettingsSVG },
+];
